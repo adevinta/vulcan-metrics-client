bda763b261f23c034cc7c928469256cfe60f535e
Daniel Jiménez <danijs12@hotmail.com>
2020-06-08 12:06:44 +0200

    Add support for clients pool (#2)

--- client.go
+++ client.go
@@ -2,21 +2,9 @@ package metrics
 
 import (
 	"errors"
-	"os"
-	"strconv"
 )
 
 const (
-	// Config env variables.
-	envClientType = "METRICS_CLIENT_TYPE"
-	envEnabled    = "METRICS_ENABLED"
-	envHost       = "METRICS_HOST"
-	envPort       = "METRICS_PORT"
-
-	// DataDogClient represents the DataDog
-	// implementation for metrics client.
-	DataDogClient = "dd"
-
 	// Count represents a count metric type.
 	Count = iota
 	// Gauge represents a gauge metric type.
@@ -35,10 +23,6 @@ var (
 // Type represents the type of metric to push.
 // Supports Count, Gauge, Histogram and Distribution types.
 type Type int
-
-// ClientType represents the requested metrics
-// client implementation.
-type ClientType string
 
 // Metric represents a metric.
 type Metric struct {
@@ -60,22 +44,7 @@ type Client interface {
 	PushWithRate(ratedMetric RatedMetric)
 }
 
-// NewClient creates a new metrics client based on given input.
-func NewClient(typ ClientType, enabled bool, host string, port int) (Client, error) {
-	switch typ {
-	case DataDogClient:
-		return newDDClient(enabled, host, port)
-	default:
-		return nil, ErrUnsupportedClientType
-	}
-}
-
-// NewClientFromEnv creates a new metrics client based on environment variables.
-func NewClientFromEnv() (Client, error) {
-	clientType := ClientType(os.Getenv(envClientType))
-	enabled, _ := strconv.ParseBool(os.Getenv(envEnabled))
-	host := os.Getenv(envHost)
-	port, _ := strconv.ParseInt(os.Getenv(envPort), 10, 0)
-
-	return NewClient(clientType, enabled, host, int(port))
+// NewClient creates a new metrics client based on environment variables.
+func NewClient() (Client, error) {
+	return newClientPool()
 }
